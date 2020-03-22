57cfcdd883301e7645176ce0fd64d513ee5f3711
Maciel Melo <maciel.melo@thoughtworks.com>
2020-03-22 18:53:22 -0300

    Enxugando alguns bits, vai qui.... é domingo...

--- src/model/news.model.js
+++
@@ -1,8 +0,0 @@
-export default obj => {
-  const data = obj || [];
-  console.log(obj);
-  // Vai retornar o objeto tratado
-  return data.map(({ link, linkImg, title }) => {
-    return { link, linkImg, title };
-  });
-};
