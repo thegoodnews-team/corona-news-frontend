0a7e7a89201dd23de1fb85b9485523a0025e3a2b
fernandolucio <fernandolucio8@gmail.com>
2020-03-27 19:39:58 -0300

    adjust

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -49,7 +49,7 @@ export default function Grid() {
     >
       <div className="album py-5">
         <div className="container">
-          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2  row-cols-lg-3 g-3">
+          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
             {visibleItems.map(item => (
               <div className="col" key={item.title}>
                 <Card key={item.title} {...item} />
