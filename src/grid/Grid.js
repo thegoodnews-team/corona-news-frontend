1b4298e64c7dcffa4a2d1c6d7f5ce839dfa2e1fe
Daniel <daniel@broomy.com.br>
2020-03-27 13:36:02 -0300

    adding initife scroll

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -1,37 +1,63 @@
-import React, { useState, useEffect } from "react";
-import csv from "csvtojson";
-import request from "request";
-import "./style.css";
-import Card from "./Card";
+import React, { useState, useEffect } from 'react';
+import csv from 'csvtojson';
+import request from 'request';
+import './style.css';
+import Card from './Card';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function Grid() {
-  const [goodNews, setGoodNews] = useState([]);
+  const BATCH_SIZE = 9;
+
+  const [allItems, setAllItems] = useState([]);
+  const [visibleItems, setVisibleItems] = useState([]);
+  const [pageIndex, setPageIndex] = useState(0);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
 
   useEffect(() => {
-    csv()
-      .fromStream(
+    const loadAllItems = async () => {
+      const json = await csv().fromStream(
         request.get(
-          "https://docs.google.com/spreadsheets/d/e/2PACX-1vS4KL9aw4PCXZ12mT_659WoihJr5Lu7xoZooXWhmcAVgNwfGqbMnX6Wk4MUxUgEYlD9XDeJ_zpXWg5n/pub?gid=0&single=true&output=csv"
+          'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4KL9aw4PCXZ12mT_659WoihJr5Lu7xoZooXWhmcAVgNwfGqbMnX6Wk4MUxUgEYlD9XDeJ_zpXWg5n/pub?gid=0&single=true&output=csv'
         )
-      )
-      .then(json => {
-        setGoodNews(json.reverse());
-      });
+      );
+      setAllItems(json.reverse());
+      setPageIndex(1);
+    };
+
+    loadAllItems();
   }, []);
 
+  useEffect(() => {
+    const start = (pageIndex - 1) * BATCH_SIZE;
+    const end = pageIndex * BATCH_SIZE;
+
+    const items = allItems.slice(start, end);
+
+    setVisibleItems(visibleItems.concat(items));
+    setHasMoreItems(allItems.length > visibleItems.length)
+  }, [pageIndex]);
+
+  const fetchMoreData = () => {
+    setPageIndex(pageIndex+1);
+  };
+
   return (
-    <>
-      <div className="album py-5 bg-light">
-        <div className="container">
-          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2  row-cols-lg-3 g-3">
-            {goodNews.map(item => (
-              <div className="col" key={item.title}>
+    <InfiniteScroll
+      dataLength={visibleItems.length}
+      next={fetchMoreData}
+      hasMore={hasMoreItems}
+    >
+      <div className='album py-5 bg-light'>
+        <div className='container'>
+          <div className='row row-cols-1 row-cols-sm-1 row-cols-md-2  row-cols-lg-3 g-3'>
+            {visibleItems.map(item => (
+              <div className='col' key={item.title}>
                 <Card key={item.title} {...item} />
               </div>
             ))}
           </div>
         </div>
       </div>
-    </>
+    </InfiniteScroll>
   );
 }
