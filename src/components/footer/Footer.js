4b6071044fb5b3c1f5e692652ea8780062ce2f92
Daniel <daniel@broomy.com.br>
2020-04-03 17:55:08 -0300

    adding aids support logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.css'
 import TW from '../../assets/tw.png'
+import AIDS from '../../assets/agencia-aids.png'
 import insta from '../../assets/insta.svg'
 import email from '../../assets/email.svg'
 import send from '../../analitycs'
@@ -11,7 +12,7 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-sm-6 text-center text-sm-left">
-            <h1 className="row-sm contact">Contato</h1>
+            <h1 className="row-sm footer-title">Contato</h1>
             <div className="row-sm social2">
               <a
                 href="https://instagram.com/thegoodnewscoronavirus"
@@ -38,18 +39,45 @@ const Footer = () => {
             </div>
           </div>
           <div className="col-xs-12 col-sm-6 text-center text-sm-left">
-            <h1 className="row-sm">Apoio</h1>
-            <a
-              className="row-sm"
-              href="https://www.thoughtworks.com/pt/remote-work-playbook"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={() =>
-                send({ name: 'TW-LOGO', category: 'FOOTER', type: 'CLICK' })
-              }
-            >
-              <img className="tw" src={TW} alt="ThoughtWorks" />
-            </a>
+            <div className="row">
+              <h1 className="col-12 footer-title">Apoio</h1>
+              <div className="col-12 mb-4">
+                <a
+                  href="https://www.thoughtworks.com/pt/remote-work-playbook"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    send({ name: 'TW-LOGO', category: 'FOOTER', type: 'CLICK' })
+                  }
+                >
+                  <img
+                    className="support-img-wide"
+                    src={TW}
+                    alt="ThoughtWorks"
+                  />
+                </a>
+              </div>
+              <div className="col-12">
+                <a
+                  href="https://agenciaaids.com.br/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    send({
+                      name: 'AIDS-LOGO',
+                      category: 'FOOTER',
+                      type: 'CLICK'
+                    })
+                  }
+                >
+                  <img
+                    className="support-img-tall"
+                    src={AIDS}
+                    alt="Agência de Notícias da AIDS"
+                  />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
