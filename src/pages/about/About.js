7d2db8490d9819a90010b40c662b6e80c8b537d9
Carolina Lucca <carolinaplucca@gmail.com>
2020-04-07 15:27:18 -0300

    Inlcusao de fotos e dados dos colaboradores do projeto

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './style.css'
 import Person from './Person'
-import Adnet from '../../assets/fernando.png'
-import Maciel from '../../assets/maciel.png'
-import Rafa from '../../assets/rafa.png'
-import Urso from '../../assets/urso.png'
+import Adnet from '../../assets/people/idealizadores/adnet.JPG'
+import Maciel from '../../assets/people/idealizadores/maciel.jpg'
+import Rafa from '../../assets/people/idealizadores/rafa.jpeg'
+import Urso from '../../assets/people/idealizadores/urso.jpeg'
+import Carol from '../../assets/people/colaboradores/carol.jpeg'
+import Yasminn from '../../assets/people/colaboradores/yasminn.jpeg'
+import Ruan from '../../assets/people/colaboradores/ruan.jpeg'
+import Rodolfo from '../../assets/people/colaboradores/rodolfo.jpeg'
+import Hiago from '../../assets/people/colaboradores/hiago.jpeg'
+import Kaio from '../../assets/people/colaboradores/kaio.jpeg'
+import Arthur from '../../assets/people/colaboradores/arthur.jpeg'
+import Jess from '../../assets/people/colaboradores/jess.jpeg'
+import Barbara from '../../assets/people/colaboradores/barbara.jpeg'
+import Yula from '../../assets/people/colaboradores/yula.jpeg'
+import Antonio from '../../assets/people/colaboradores/antonio.jpeg'
+import Roberto from '../../assets/people/colaboradores/roberto.jpeg'
 
 const About = () => {
   return (
@@ -34,7 +46,7 @@ const About = () => {
       </div>
 
       <div className="subtitle">
-        <h2>Quem somos</h2>
+        <h2>Idealizadores</h2>
         <div className="row mt-5">
           <Person
             name="Fernando Fernandes"
@@ -60,6 +72,84 @@ const About = () => {
             link="https://www.linkedin.com/in/danielnbarros/"
             linkImg={Urso}
           />
+        </div>
+
+        <div className="subtitle">
+          <h2>Colaboradores</h2>
+          <div className="row mt-5">
+            <Person
+              name="Carolina Lucca"
+              role="Desenvolvedora"
+              link="https://www.linkedin.com/in/carolina-lucca/"
+              linkImg={Carol}
+            />
+            <Person
+              name="Yasminn Vaz"
+              role="Desenvolvedora"
+              link="https://www.linkedin.com/in/yasminnvaz/"
+              linkImg={Yasminn}
+            />
+            <Person
+              name="Ruan Victor"
+              role="Desenvolvedor"
+              link="https://www.linkedin.com/in/ruanvictor/"
+              linkImg={Ruan}
+            />
+            <Person
+              name="Rodolfo Santos"
+              role="Desenvolvedor"
+              link="https://www.linkedin.com/in/rodolfosvc/"
+              linkImg={Rodolfo}
+            />
+            <Person
+              name="Hiago Balbino"
+              role="Desenvolvedor"
+              link="https://www.linkedin.com/in/hiagobalbino"
+              linkImg={Hiago}
+            />
+            <Person
+              name="Kaio Oliveira de Morais"
+              role="Desenvolvedor"
+              link="https://www.linkedin.com/in/kaioramon/"
+              linkImg={Kaio}
+            />
+            <Person
+              name="Arthur Cohen"
+              role="Desenvolvedor"
+              link="https://www.linkedin.com/in/forcohen/"
+              linkImg={Arthur}
+            />
+            <Person
+              name="Jéssica Pureza"
+              role="Visual Designer"
+              link="https://br.linkedin.com/in/j%C3%A9ssica-pureza-206221170"
+              linkImg={Jess}
+            />
+            <Person
+              name="Bárbara Jaber"
+              role="Desenvolvedora"
+              link="https://www.linkedin.com/in/barbara-poliana-jaber-m-ferreira-193559b9"
+              linkImg={Barbara}
+            />
+            <Person
+              name="Yula Rocha"
+              role="Jornalista"
+              link="https://www.linkedin.com/in/yula-rocha-70797370/"
+              linkImg={Yula}
+            />
+            <Person
+              name="Antônio Aureliano"
+              role="Analista de Qualidade"
+              link="https://www.linkedin.com/in/antonioaureliano/"
+              linkImg={Antonio}
+            />
+            <Person
+              name="Roberto Oliveira"
+              role="Desenvolvedor"
+              link="https://www.linkedin.com/in/roberto-oliveira-998a83a8/"
+              linkImg={Roberto}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/about/Person.js
+++ src/pages/about/Person.js
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types'
 
 const Person = ({ name, role, link, linkImg }) => {
   return (
-    <div className='col-xs-12 col-md-6 mb-4'>
-      <div className='row text-center text-md-left'>
+    <div className='col-xs-12 col-md-3 mb-4'>
+      <div className='row d-flex align-items-center justify-content-center'>
         <a className='col-xs-12 col-md-auto' href={link} target='_blank' rel='noopener noreferrer'
           onClick={() => send({ name: name, category: 'LINKEDIN', type: 'CLICK' })}>
           <img className='img' src={linkImg} alt={`Foto de ${name}`} />
         </a>
-        <div className='col-xs-12 col-md-auto'>
+        <div className='col-xs-12 col-md-auto text-center'>
           <p className='name'>{name}</p>
           <p className='role'>{role}</p>
         </div>
