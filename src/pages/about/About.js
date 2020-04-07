f854328a6cc11d8f83fd5750c3b45b1cd3b582f0
Carolina Lucca <carolinaplucca@gmail.com>
2020-04-07 19:28:46 -0300

    Refatoraçao da exibiçao de pessoas no sobre e alteraçao do layout da nota

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,42 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import Person from './Person'
-import Adnet from '../../assets/people/idealizadores/adnet.JPG'
-import Maciel from '../../assets/people/idealizadores/maciel.jpg'
-import Rafa from '../../assets/people/idealizadores/rafa.jpeg'
-import Urso from '../../assets/people/idealizadores/urso.jpeg'
-import Carol from '../../assets/people/colaboradores/carol.jpeg'
-import Yasminn from '../../assets/people/colaboradores/yasminn.jpeg'
-import Ruan from '../../assets/people/colaboradores/ruan.jpeg'
-import Rodolfo from '../../assets/people/colaboradores/rodolfo.jpeg'
-import Hiago from '../../assets/people/colaboradores/hiago.jpeg'
-import Kaio from '../../assets/people/colaboradores/kaio.jpeg'
-import Arthur from '../../assets/people/colaboradores/arthur.jpeg'
-import Jess from '../../assets/people/colaboradores/jess.jpeg'
-import Barbara from '../../assets/people/colaboradores/barbara.jpeg'
-import Yula from '../../assets/people/colaboradores/yula.jpeg'
-import Antonio from '../../assets/people/colaboradores/antonio.jpeg'
-import Roberto from '../../assets/people/colaboradores/roberto.jpeg'
 import getItemsFromSpreadsheet from '../../utils/spreadsheet'
 import content from '../../utils/content'
 
-// const [creators, setCreators] = useState([])
-// const [colaborators, setColaborators] = useState([])
-
 const About = () => {
-  const { people } = content
+  const { people: link } = content
+
+  const [creators, setCreators] = useState([])
+  const [colaborators, setColaborators] = useState([])
 
   useEffect(() => {
     const loadPeople = async () => {
-      const peopleJson = await getItemsFromSpreadsheet(people)
-      console.log(peopleJson)
+      const peopleJson = await getItemsFromSpreadsheet(link)
+      listOfPeople(peopleJson)
     }
     loadPeople()
   }, [])
 
+  function listOfPeople(peopleJson) {
+    const creatorsList = peopleJson.filter(creator => creator.Role === 'Idealizador')
+    const colaboratorsList = peopleJson.filter(colaborator => colaborator.Role === 'Colaborador')
+    setCreators(creatorsList)
+    setColaborators(colaboratorsList)
+  }
+
   return (
     <div className="container">
-      <h1 className="title">Sobre</h1>
       <div className="subtitle">
         <h2>Nossa missão</h2>
         <p className="description">
@@ -48,13 +38,11 @@ const About = () => {
         <p className="description">
           Através desse portal a sociedade vai saber que existe um amanhã de possibilidades e que a derrota ao Coronavírus vai entrar na história como o momento em que nos conectamos  e construímos um futuro melhor.
         </p>
-      </div>
-
-      <div className="subtitle">
-        <h2>Nota</h2>
-        <p className="description">
-          Esse portal não tem qualquer direcionamento político, nenhum relacionamento ou suporte vindo de qualquer nível de governo ou seus terceiros, tendo sua idealização e desenvolvimento inteiramente partido de membros da sociedade civil.
-        </p>
+        <blockquote>
+          <p className="description note">
+            Esse portal não tem qualquer direcionamento político, nenhum relacionamento ou suporte vindo de qualquer nível de governo ou seus terceiros, tendo sua idealização e desenvolvimento inteiramente partido de membros da sociedade civil.
+          </p>
+        </blockquote>
         <p className="description">
           As pessoas  desse grupo são favoráveis ao isolamento social como melhor medida para o combate ao Covid-19 e defendem que todas as medidas e recomendação da Organização Mundial de Saúde (OMS) no combate ao Covid-19 sejam seguidas.
         </p>
@@ -63,107 +51,27 @@ const About = () => {
       <div className="subtitle">
         <h2>Idealizadores</h2>
         <div className="row mt-5">
-          <Person
-            name="Fernando Fernandes"
-            role="Desenvolvedor"
-            link="https://www.linkedin.com/in/flasfl/"
-            linkImg={Adnet}
-          />
-          <Person
-            name="Rafaela Fernandes"
-            role="Designer"
-            link="https://www.linkedin.com/in/rafaela-fernandes/"
-            linkImg={Rafa}
-          />
-          <Person
-            name="Maciel Melo"
-            role="Desenvolvedor"
-            link="https://www.linkedin.com/in/macielmelo/"
-            linkImg={Maciel}
-          />
-          <Person
-            name="Daniel Barros"
-            role="Desenvolvedor"
-            link="https://www.linkedin.com/in/danielnbarros/"
-            linkImg={Urso}
-          />
+          {creators.map(creator => (
+            <Person key="idealizadores"
+              name={creator.Nome}
+              role={creator.Cargo}
+              link={creator.Link}
+              linkImg={creator.Imagem}
+            />
+          ))}
         </div>
 
         <div className="subtitle">
           <h2>Colaboradores</h2>
           <div className="row mt-5">
-            <Person
-              name="Carolina Lucca"
-              role="Desenvolvedora"
-              link="https://www.linkedin.com/in/carolina-lucca/"
-              linkImg={Carol}
-            />
-            <Person
-              name="Yasminn Vaz"
-              role="Desenvolvedora"
-              link="https://www.linkedin.com/in/yasminnvaz/"
-              linkImg={Yasminn}
-            />
-            <Person
-              name="Ruan Victor"
-              role="Desenvolvedor"
-              link="https://www.linkedin.com/in/ruanvictor/"
-              linkImg={Ruan}
-            />
-            <Person
-              name="Rodolfo Santos"
-              role="Desenvolvedor"
-              link="https://www.linkedin.com/in/rodolfosvc/"
-              linkImg={Rodolfo}
-            />
-            <Person
-              name="Hiago Balbino"
-              role="Desenvolvedor"
-              link="https://www.linkedin.com/in/hiagobalbino"
-              linkImg={Hiago}
-            />
-            <Person
-              name="Kaio Oliveira de Morais"
-              role="Desenvolvedor"
-              link="https://www.linkedin.com/in/kaioramon/"
-              linkImg={Kaio}
-            />
-            <Person
-              name="Arthur Cohen"
-              role="Desenvolvedor"
-              link="https://www.linkedin.com/in/forcohen/"
-              linkImg={Arthur}
-            />
-            <Person
-              name="Jéssica Pureza"
-              role="Visual Designer"
-              link="https://br.linkedin.com/in/j%C3%A9ssica-pureza-206221170"
-              linkImg={Jess}
-            />
-            <Person
-              name="Bárbara Jaber"
-              role="Desenvolvedora"
-              link="https://www.linkedin.com/in/barbara-poliana-jaber-m-ferreira-193559b9"
-              linkImg={Barbara}
-            />
-            <Person
-              name="Yula Rocha"
-              role="Jornalista"
-              link="https://www.linkedin.com/in/yula-rocha-70797370/"
-              linkImg={Yula}
-            />
-            <Person
-              name="Antônio Aureliano"
-              role="Analista de Qualidade"
-              link="https://www.linkedin.com/in/antonioaureliano/"
-              linkImg={Antonio}
-            />
-            <Person
-              name="Roberto Oliveira"
-              role="Desenvolvedor"
-              link="https://www.linkedin.com/in/roberto-oliveira-998a83a8/"
-              linkImg={Roberto}
-            />
+            {colaborators.map(colaborator => (
+              <Person key="colaboradores"
+                name={colaborator.Nome}
+                role={colaborator.Cargo}
+                link={colaborator.Link}
+                linkImg={colaborator.Imagem}
+              />
+            ))}
           </div>
         </div>
       </div>
