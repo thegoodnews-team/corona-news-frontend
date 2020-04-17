0a5830ab25f99090b656745e8a1b9b73d54d1ef7
Daniel <daniel@broomy.com.br>
2020-04-17 16:26:31 -0300

    adding adsense card

--- src/components/cards-grid/CardsGrid.js
+++ src/components/cards-grid/CardsGrid.js
@@ -4,6 +4,7 @@ import Card from './Card'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import PropTypes from 'prop-types'
 import Loading from '../loading'
+import AdSense from 'react-adsense'
 
 export default function CardsGrid({ content, analyticsCategory }) {
   const BATCH_SIZE = 9
@@ -43,16 +44,37 @@ export default function CardsGrid({ content, analyticsCategory }) {
         <div className="album py-5">
           <div className="container">
             <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
-              {visibleItems.map(item => (
-                <div className="col" key={`${item.title}-${item.fonte}`}>
-                  <Card {...item} analyticsCategory={analyticsCategory} />
-                </div>
+              {visibleItems.map((item, index) => (
+                <>
+                  <div className="col" key={`${item.title}-${item.fonte}`}>
+                    <Card {...item} analyticsCategory={analyticsCategory} />
+                  </div>
+                  {buildAd(index, `${item.title}-${item.fonte}ad`)}
+                </>
               ))}
             </div>
           </div>
         </div>
       </InfiniteScroll>
     )
+  }
+
+  function buildAd(index, key) {
+    if (index % 2 === 1) {
+      return (
+        <div className="col" key={key}>
+          <AdSense.Google
+            client="ca-pub-2186608926010606"
+            slot="6057077357"
+            style={{ display: 'block' }}
+            layoutKey="-6g+bz+1g-9+dz"
+            format="fluid"
+          />
+        </div>
+      )
+    } else {
+      return null
+    }
   }
 
   return allItems.length > 0 ? buildContent() : <Loading />
