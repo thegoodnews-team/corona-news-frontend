c2bb008e650e0723a5a1182753a72d490d580de7
Carolina Lucca <carolinaplucca@gmail.com>
2020-05-05 19:08:22 -0300

    Ajustes consulta API com novo codigo

--- src/utils/FormatNumber.js
+++ src/utils/FormatNumber.js
@@ -2,6 +2,11 @@ function format(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ')
 }
 
+function removeComma(num) {
+  return num.toString().replace(/\D+/g, '')
+}
+
 export default function formatNumber(num) {
-  return format(num)
+  var number = removeComma(num)
+  return format(number)
 }
