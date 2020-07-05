c9fc8036500148c9dd2547e2d9addaa1e45cc47c
Daniel Barros <danielnbarros@users.noreply.github.com>
2020-07-05 18:35:15 -0300

    adjusted to use google sheets api

--- src/utils/fetchUrl.js
+++ src/utils/fetchUrl.js
@@ -1,6 +1,9 @@
+import Tabletop from 'tabletop'
 
-export default async function getItemsFromSpreadsheet(link) {
-  const response = await fetch(link)
-  const data = await response.json()
-  return data
+export default function getItemsFromSpreadsheet(sheet) {
+  return Tabletop.init({
+    key: '1uGAZWgEeNEEWXu-CI8Di95cCq6CidFy7LUlH2KQluZ0',
+    simpleSheet: true,
+    wanted: [sheet]
+  }).then((data, tabletop) => data)
 }
