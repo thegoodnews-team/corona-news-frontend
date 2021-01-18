7c79c340dfd7236068c73e4ebfa4b5e27f545f00
Feferna <fernando_fernandes@gap.com>
2021-01-18 19:55:39 -0300

    fonte

--- src/pages/home/VaccineStatus.js
+++ src/pages/home/VaccineStatus.js
@@ -10,25 +10,29 @@ export default function VaccineStatus({ numOfDosesBrazil, numOfDosesWorld }) {
   const lang = localStorage.getItem('goodnewscoronavirus')
 
   const brazilCount = (
-    <h1 className="pb-4 fw-light">
-      <img
-        src={vaccine}
-        alt="Vacina"
-        style={{ fill: 'black', width: '50px', height: '50px', marginRight: '10px', marginTop: '-10px' }}
-      />
-      <CountUp start={0} end={numOfDosesBrazil} /> {vaccineProp.brazil}
-    </h1>
+    <a style={{ color: 'black' }} href='https://ourworldindata.org/grapher/cumulative-covid-vaccinations?tab=chart&stackMode=absolute&time=2020-12-26..latest&region=World' rel='noopener noreferrer' target='_blank'>
+      <h1 className="pb-4 fw-light">
+        <img
+          src={vaccine}
+          alt="Vacina"
+          style={{ fill: 'black', width: '50px', height: '50px', marginRight: '10px', marginTop: '-10px' }}
+        />
+        <CountUp start={0} end={numOfDosesBrazil} /> {vaccineProp.brazil}
+      </h1>
+    </a>
   )
 
   const worldCount = (
-    <h1 className="pb-4 fw-light">
-      <img
-        src={vaccine}
-        alt="Vacina"
-        style={{ fill: 'black', width: '50px', height: '50px', marginRight: '10px', marginTop: '-10px' }}
-      />
-      <CountUp start={0} end={numOfDosesWorld} /> {vaccineProp.world}
-    </h1>
+    <a style={{ color: 'black' }} href='https://ourworldindata.org/grapher/cumulative-covid-vaccinations?tab=chart&stackMode=absolute&time=2020-12-26..latest&region=World' rel='noopener noreferrer' target='_blank'>
+      <h1 className="pb-4 fw-light">
+        <img
+          src={vaccine}
+          alt="Vacina"
+          style={{ fill: 'black', width: '50px', height: '50px', marginRight: '10px', marginTop: '-10px' }}
+        />
+        <CountUp start={0} end={numOfDosesWorld} /> {vaccineProp.world}
+      </h1>
+    </a>
   )
 
   return (
